35e4b13dc492883a858ead0faf1f13fe7b2f1ca6
psychopathh <sn05coast@gmail.com>
2021-04-09 22:33:34 +0300

    geometry

--- src/react-goban.jsx
+++ src/react-goban.jsx
@@ -1,3 +1,19 @@
+"use strict";
+
+var _extends =
+  Object.assign ||
+  function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+    return target;
+  };
+
 /**
  * react-goban
  * react-goban.jsx
@@ -6,10 +22,9 @@
 
 /** @todo Split into multiple files (one per class)? */
 
-
-var React = require('react');
-var ReactDOM = require('react-dom');
-var SVGoban = require('svgoban');
+var React = require("react");
+var ReactDOM = require("react-dom");
+var SVGoban = require("svgoban");
 
 /**
  * Converts shape list into React SVG elements.
@@ -18,79 +33,142 @@ var SVGoban = require('svgoban');
  * @returns {Array} React element list
  */
 function toElem(shapes, callback) {
-    var typeofShape;
-    var txt = null;
-    var k = 0;
-    for (var i = 0; i < shapes.length; i++) {
-	typeofShape = shapes[i].type;
-	if (typeofShape == "text") {
-	    txt = shapes[i].txt;
-	    delete shapes[i].txt;
-	}
-	if (shapes[i].class) {
-	    shapes[i].className = shapes[i].class
-	    delete shapes[i].class;
-	}
-	delete shapes[i].type;
-	shapes[i].key = shapes[i].key || k++;
-	if (callback) shapes[i].onClick=callback.bind(null, shapes[i].key); // Replace this by null for React
-	shapes[i] = React.createElement(typeofShape, shapes[i], txt);
+  var typeofShape;
+  var txt = null;
+  var k = 0;
+  for (var i = 0; i < shapes.length; i++) {
+    typeofShape = shapes[i].type;
+    if (typeofShape == "text") {
+      txt = shapes[i].txt;
+      delete shapes[i].txt;
     }
-    return shapes;
+    if (shapes[i].class) {
+      shapes[i].className = shapes[i].class;
+      delete shapes[i].class;
+    }
+    delete shapes[i].type;
+    shapes[i].key = shapes[i].key || k++;
+    if (callback) shapes[i].onClick = callback.bind(null, shapes[i].key); // Replace this by null for React
+    shapes[i] = React.createElement(typeofShape, shapes[i], txt);
+  }
+  return shapes;
 }
 
 var LabelsLayer = React.createClass({
-    shouldComponentUpdate: function(nextProps, nextState) {
-	return ((nextProps.size !== this.props.size) || (nextProps.coordSystem !== this.props.coordSystem));
-    },
-    render: function() {
-	var pseudoLabels = SVGoban.shapeLabels(this.props.size, this.props.coordSystem);
-	return (
-		<g className="labels_layer">{toElem(pseudoLabels)}</g>
-	);
-    }
+  displayName: "LabelsLayer",
+
+  shouldComponentUpdate: function shouldComponentUpdate(nextProps, nextState) {
+    return (
+      nextProps.size !== this.props.size ||
+      nextProps.coordSystem !== this.props.coordSystem
+    );
+  },
+  render: function render() {
+    var pseudoLabels = SVGoban.shapeLabels(
+      this.props.size,
+      this.props.coordSystem
+    );
+    return React.createElement(
+      "g",
+      { className: "labels_layer" },
+      toElem(pseudoLabels)
+    );
+  },
 });
 
 var BackgroundLayer = React.createClass({
-    render: function() {
-	var pseudoBackground = SVGoban.shapeBackground(this.props.noMargin);
-	return (
-		<g className="background_layer">{toElem(pseudoBackground)}</g>
-	);
-    }
+  displayName: "BackgroundLayer",
+
+  render: function render() {
+    var pseudoBackground = SVGoban.shapeBackground(this.props.noMargin);
+    return React.createElement(
+      "g",
+      { className: "background_layer" },
+      toElem(pseudoBackground)
+    );
+  },
 });
 
 var GridLayer = React.createClass({
-    shouldComponentUpdate: function(nextProps, nextState) {
-	return (nextProps.size !== this.props.size);
-    },
-    render: function() {
-	var pseudoLines = SVGoban.shapeGrid(this.props.size);
-	return (
-		<g className="grid_layer">{toElem(pseudoLines)}</g>
-	);
-    }
+  displayName: "GridLayer",
+
+  shouldComponentUpdate: function shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.size !== this.props.size;
+  },
+  render: function render() {
+    var pseudoLines = SVGoban.shapeGrid(this.props.size);
+    return React.createElement(
+      "g",
+      { className: "grid_layer" },
+      toElem(pseudoLines)
+    );
+  },
 });
 
 var StarPointsLayer = React.createClass({
-    shouldComponentUpdate: function(nextProps, nextState) {
-	return (nextProps.size !== this.props.size);
-    },
-    render: function() {
-	var pseudoStarPoints = SVGoban.shapeStarPoints(this.props.size);
-	return (
-		<g className="starpoints_layer">{toElem(pseudoStarPoints)}</g>
-	);
-    }
+  displayName: "StarPointsLayer",
+
+  shouldComponentUpdate: function shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.size !== this.props.size;
+  },
+  render: function render() {
+    var pseudoStarPoints = SVGoban.shapeStarPoints(this.props.size);
+    return React.createElement(
+      "g",
+      { className: "starpoints_layer" },
+      toElem(pseudoStarPoints)
+    );
+  },
 });
 
 var MarkersLayer = React.createClass({
-    render: function() {
-	var pseudoMarkers = SVGoban.shapeMarkers(this.props.size, this.props.markers, this.props.positions);
-	return (
-		<g className="markers_layer">{toElem(pseudoMarkers)}</g>
-	);
+  handleClick: function handleClick(intersection) {
+    const arr = [];
+    for (let i in this.props.markers) {
+      arr.push(i);
     }
+    this.props.onIntersectionClick(arr[intersection]);
+  },
+  displayName: "MarkersLayer",
+
+  render: function render() {
+    var pseudoMarkers = SVGoban.shapeMarkers(
+      this.props.size,
+      this.props.markers,
+      this.props.positions,
+      ['redMarker']
+    );
+    return React.createElement(
+      "g",
+      { className: "markers_layer" },
+      toElem(pseudoMarkers, this.handleClick)
+    );
+  },
+});
+var MapLayer = React.createClass({
+  handleClick: function handleClick(intersection) {
+    const arr = [];
+    for (let i in this.props.mapStones) {
+      arr.push(i);
+    }
+    this.props.onIntersectionClick(arr[intersection]);
+  },
+  displayName: "mapLayer",
+
+  render: function render() {
+    var pseudoMarkers = SVGoban.mapMarkers(
+      this.props.size,
+      this.props.mapStones,
+      this.props.positions,
+      this.props.classNamesMapStones
+    );
+    return React.createElement(
+      "g",
+      { className: "map_layer" },
+      toElem(pseudoMarkers, this.handleClick)
+    );
+    
+  },
 });
 
 /**
@@ -98,15 +176,19 @@ var MarkersLayer = React.createClass({
  *
  */
 var FlatStonesLayer = React.createClass({
-    handleClick: function(intersection) {
-	this.props.onIntersectionClick(intersection);
-    },
-    render: function() {
-	var pseudoStones = SVGoban.shapeStones(this.props.size, this.props.set);
-	return (
-		<g className="stones_layer">{toElem(pseudoStones, this.handleClick)}</g>
-	);
-    }
+  displayName: "FlatStonesLayer",
+
+  handleClick: function handleClick(intersection) {
+    this.props.onIntersectionClick(intersection);
+  },
+  render: function render() {
+    var pseudoStones = SVGoban.shapeStones(this.props.size, this.props.set);
+    return React.createElement(
+      "g",
+      { className: "stones_layer" },
+      toElem(pseudoStones, this.handleClick)
+    );
+  },
 });
 
 /**
@@ -114,109 +196,217 @@ var FlatStonesLayer = React.createClass({
  *
  */
 var CompositeStonesLayer = React.createClass({
-    handleClick: function(intersection) {
-	this.props.onIntersectionClick(intersection);
-    },
-    render: function() {
-	var i, j, skipI, hA1, vA1, haa, vaa, coordA1, coordaa, color;
-	var size = +this.props.size;
-	var items = [];
+  displayName: "CompositeStonesLayer",
 
-	for (i = 1; i <= size; i++) {
-	    skipI = i >= 9 ? 1 : 0;
-	    hA1 = String.fromCharCode(64 + i + skipI);
-	    haa = String.fromCharCode(96 + i);
-	    for (j = 1; j <= size; j++) {
-		vA1 = j.toString();
-		vaa = String.fromCharCode(96 + size - j + 1);
-		coordA1 = hA1 + vA1;
-		coordaa = haa + vaa;
-		color = this.props.set[coordA1] || this.props.set[coordaa] || "placeholder";
-		items.push(<Stone key={coordA1} size={this.props.size} intersection={coordA1} 
-			   color={color} onIntersectionClick={this.handleClick} />);
-	    }
-	}
-	var cls = "stones_layer " + this.props.nextToPlay;
-	return (
-		<g className={cls}>{items}</g>
-	);
+  handleClick: function handleClick(intersection) {
+    this.props.onIntersectionClick(intersection);
+  },
+  render: function render() {
+    var i, j, skipI, hA1, vA1, haa, vaa, coordA1, coordaa, color;
+    var size = +this.props.size;
+    var items = [];
+
+    for (i = 1; i <= size; i++) {
+      skipI = i >= 9 ? 1 : 0;
+      hA1 = String.fromCharCode(64 + i + skipI);
+      haa = String.fromCharCode(96 + i);
+      for (j = 1; j <= size; j++) {
+        vA1 = j.toString();
+        vaa = String.fromCharCode(96 + size - j + 1);
+        coordA1 = hA1 + vA1;
+        coordaa = haa + vaa;
+        color =
+          this.props.set[coordA1] || this.props.set[coordaa] || "placeholder";
+        items.push(
+          React.createElement(Stone, {
+            key: coordA1,
+            size: this.props.size,
+            intersection: coordA1,
+            color: color,
+            onIntersectionClick: this.handleClick,
+          })
+        );
+      }
     }
+    var cls = "stones_layer " + this.props.nextToPlay;
+    return React.createElement("g", { className: cls }, items);
+  },
 });
 
 var Stone = React.createClass({
-    shouldComponentUpdate: function(nextProps, nextState) {
-	var idem = (nextProps.size === this.props.size 
-		    && nextProps.intersection === this.props.intersection 
-		    && nextProps.color === this.props.color);
-	return !idem;
-    },
-    render: function() {
-	var pseudoStone = SVGoban.shapeStone(this.props.size, this.props.intersection, this.props.color);
-	return (
-		toElem(pseudoStone, this.props.onIntersectionClick)[0]
-	);
-    }
+  displayName: "Stone",
+
+  shouldComponentUpdate: function shouldComponentUpdate(nextProps, nextState) {
+    var idem =
+      nextProps.size === this.props.size &&
+      nextProps.intersection === this.props.intersection &&
+      nextProps.color === this.props.color;
+    return !idem;
+  },
+  render: function render() {
+    var pseudoStone = SVGoban.shapeStone(
+      this.props.size,
+      this.props.intersection,
+      this.props.color
+    );
+    return toElem(pseudoStone, this.props.onIntersectionClick)[0];
+  },
 });
 
 var Style = React.createClass({
-    shouldComponentUpdate: function(nextProps, nextState) {
-	return (nextProps.theme !== this.props.theme);
-    },
-    render: function() {
-	return (
-		<style>{SVGoban.Themes[this.props.theme]()}</style>
-	);
-    }
+  displayName: "Style",
+
+  shouldComponentUpdate: function shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.theme !== this.props.theme;
+  },
+  render: function render() {
+    return React.createElement(
+      "style",
+      null,
+      SVGoban.Themes[this.props.theme]()
+    );
+  },
 });
 
 var Definitions = React.createClass({
-    shouldComponentUpdate: function(nextProps, nextState) {
-	return false;
-    },
-    render: function() {
-	var b = SVGoban.defineRadialColors("black");
-	var w = SVGoban.defineRadialColors("white");
-	return (
-		<defs>
-		  <radialGradient id={"blackgrad"} {...b.gradient}>
-		    <stop offset="0%" style={{"stop-color": b.a, "stop-opacity":"1"}}/>
-		    <stop offset="100%" style={{"stop-color": b.z, "stop-opacity":"1"}}/>
-		  </radialGradient>
-		  <radialGradient id={"whitegrad"} {...w.gradient}>
-		    <stop offset="0%" style={{"stop-color": w.a, "stop-opacity":"1"}}/>
-		    <stop offset="100%" style={{"stop-color": w.z, "stop-opacity":"1"}}/>
-		  </radialGradient>
-		</defs>
-	);
-    }
+  displayName: "Definitions",
+
+  shouldComponentUpdate: function shouldComponentUpdate(nextProps, nextState) {
+    return false;
+  },
+  render: function render() {
+    var b = SVGoban.defineRadialColors("black");
+    var w = SVGoban.defineRadialColors("white");
+    var x = SVGoban.defineRadialColors("red");
+    var g = SVGoban.defineRadialColors("green");
+    var bl = SVGoban.defineRadialColors("blue");
+    return React.createElement(
+      "defs",
+      null,
+      React.createElement(
+        "radialGradient",
+        _extends({ id: "blackgrad" }, b.gradient),
+        React.createElement("stop", {
+          offset: "0%",
+          style: { "stop-color": b.a, "stop-opacity": "1" },
+        }),
+        React.createElement("stop", {
+          offset: "100%",
+          style: { "stop-color": b.z, "stop-opacity": "1" },
+        })
+      ),
+      React.createElement(
+        "radialGradient",
+        _extends({ id: "whitegrad" }, w.gradient),
+        React.createElement("stop", {
+          offset: "0%",
+          style: { "stop-color": w.a, "stop-opacity": "1" },
+        }),
+        React.createElement("stop", {
+          offset: "100%",
+          style: { "stop-color": w.z, "stop-opacity": "1" },
+        })
+      ),
+      React.createElement(
+        "radialGradient",
+        _extends({ id: "redgrad" }, x.gradient),
+        React.createElement("stop", {
+          offset: "0%",
+          style: { "stop-color": x.a, "stop-opacity": "1" },
+        }),
+        React.createElement("stop", {
+          offset: "100%",
+          style: { "stop-color": x.z, "stop-opacity": "1" },
+        })
+      ),
+      React.createElement(
+        "radialGradient",
+        _extends({ id: "greengrad" }, g.gradient),
+        React.createElement("stop", {
+          offset: "0%",
+          style: { "stop-color": g.a, "stop-opacity": "1" },
+        }),
+        React.createElement("stop", {
+          offset: "100%",
+          style: { "stop-color": g.z, "stop-opacity": "1" },
+        })
+      ),
+      React.createElement(
+        "radialGradient",
+        _extends({ id: "bluegrad" }, bl.gradient),
+        React.createElement("stop", {
+          offset: "0%",
+          style: { "stop-color": bl.a, "stop-opacity": "1" },
+        }),
+        React.createElement("stop", {
+          offset: "100%",
+          style: { "stop-color": bl.z, "stop-opacity": "1" },
+        })
+      ),
+    );
+  },
 });
 
+/** @todo Add property to handle SVG className (next color to play) */
+
 var Goban = React.createClass({
-    getDefaultProps: function() {
-	return {
-	    size: "13",
-	    theme: "classic"
-	};
-    },
-    render: function() {
-	var viewBox = SVGoban.shapeArea(this.props.hideBorder, this.props.zoom, this.props.size).join(" ");
-	return (
-		<div className="react-goban">
-		  <svg className="svgoban" viewBox={viewBox} xmlns="http://www.w3.org/2000/svg" version="1.1" height="100%"> 
-		    <Style theme={this.props.theme}/>
-		    <Definitions/>
-		    <BackgroundLayer noMargin={this.props.noMargin}/>
-		    <GridLayer size={this.props.size}/>
-		    <StarPointsLayer size={this.props.size}/>
-		    <LabelsLayer size={this.props.size} coordSystem={this.props.coordSystem}/>
-		    <CompositeStonesLayer size={this.props.size} set={this.props.stones} 
-	               nextToPlay={this.props.nextToPlay} onIntersectionClick={this.props.onIntersectionClick}/>
-		    <MarkersLayer size={this.props.size} markers={this.props.markers} positions={this.props.stones}/>
-		  </svg>
-		</div>
-	);
-    }
+  displayName: "Goban",
+
+  getDefaultProps: function getDefaultProps() {
+    return {
+      size: "13",
+      theme: "classic",
+    };
+  },
+  render: function render() {
+    var viewBox = SVGoban.shapeArea(
+      this.props.hideBorder,
+      this.props.zoom,
+      this.props.size
+    ).join(" ");
+    return React.createElement(
+      "div",
+      { className: "react-goban" },
+      React.createElement(
+        "svg",
+        {
+          className: "svgoban",
+          viewBox: viewBox,
+          xmlns: "http://www.w3.org/2000/svg",
+          version: "1.1",
+          height: "100%",
+        },
+        React.createElement(Style, { theme: this.props.theme }),
+        React.createElement(Definitions, null),
+        React.createElement(BackgroundLayer, { noMargin: this.props.noMargin }),
+        React.createElement(GridLayer, { size: this.props.size }),
+        React.createElement(StarPointsLayer, { size: this.props.size }),
+        React.createElement(LabelsLayer, {
+          size: this.props.size,
+          coordSystem: this.props.coordSystem,
+        }),
+        React.createElement(CompositeStonesLayer, {
+          size: this.props.size,
+          set: this.props.stones,
+          nextToPlay: this.props.nextToPlay,
+          onIntersectionClick: this.props.onIntersectionClick,
+        }),
+        React.createElement(MarkersLayer, {
+          size: this.props.size,
+          markers: this.props.markers,
+          positions: this.props.stones,
+          onIntersectionClick: this.props.onIntersectionClick,
+        }),
+        React.createElement(MapLayer, {
+          size: this.props.size,
+          mapStones: this.props.mapStones,
+          positions: this.props.stones,
+          onIntersectionClick: this.props.onIntersectionClick,
+          classNamesMapStones: this.props.classNamesMapStones,
+        })
+      )
+    );
+  },
 });
 
 exports.Goban = Goban;
-
